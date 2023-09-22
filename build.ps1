bbd152371e4df8d6b4c67cb70cf06ffbb7077e8e
Yaroslav Bugaria <yrs.raider@gmail.com>
2023-09-21 22:51:57 -0700

    Override native menu + update build

--- build.ps1
+++ build.ps1
@@ -4,6 +4,20 @@ Push-Location $PSScriptRoot
 
 try
 {
+    if ([System.Environment]::OSVersion.Platform -eq [PlatformID]::Win32NT) {
+
+        $Cygwin = "$env:SystemDrive\cygwin64\bin";
+
+        if (-not (Test-Path $Cygwin)) {
+            Write-Error "Cygwin can not be found or not installed."
+            exit -1
+        }
+
+        if (($env:Path -split ';') -notcontains $Cygwin) {
+            $env:Path = $env:Path.TrimEnd(';') + ";" + $Cygwin
+        }
+    }
+
     $Version = ./get-version.ps1
 
     Write-Host -ForegroundColor Yellow "`r`n#### Test ####`r`n"
@@ -22,6 +36,7 @@ try
             -p:PublishProfile=$Profile -p:VersionPrefix="$($Version.Prefix)" -p:VersionSuffix="$($Version.Suffix)" -c Release `
             -o "build/$Profile" -p:DebugType=None -p:DebugSymbols=false
         if ($LASTEXITCODE -ne 0) { exit $LASTEXITCODE }
+
     }
 
     dir "build" -Include *.zip, *.tgz -Recurse | move -Destination "build" -Force
